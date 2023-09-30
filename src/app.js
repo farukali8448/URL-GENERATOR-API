9d94d51c7c467a89d059d290a1da19e73b1103d1
tanmoy119 <srroy119@gmail.com>
2023-09-30 13:03:47 +0530

    Structure

---
+++ src/app.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+
+//! Express.json-----------------
+app.use(express.json());
+
+
+//!DATABASE Connection
+require("./db/con");
+
+
+//! PORT
+const PORT = process.env.PORT || 3000;
+
+//!Use Router--------
+
+
+//! Page Not Found-------------
+app.use("*",(req,res,next)=>{
+    res.status(404).json({error:false,message:"404 Page Not Found"});
+})
+
+//! error handling middleware ------------------
+
+app.use((error,req,res,next)=>{
+    res.status(401).json({error:true,message:error.message});
+})
+
+
+app.listen(PORT, (err)=>{
+    if(err) throw err;
+    console.log(`Server running on PORT ${PORT}`);
+})
